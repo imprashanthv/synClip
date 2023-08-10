7e17d2425365b82aee8fc101456407432d934c08
imprashanthv <vissaprashanth@gmail.com>
2023-08-10 15:38:53 -0400

    Initial commit

---
+++ MacOS Application/main.py
@@ -0,0 +1,7 @@
+from ClipBoard import ClipBoard
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    clipboard_manager = ClipBoard()
+    clipboard_manager.run()
+
